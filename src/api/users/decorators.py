273434019876cdee2ce89156a2097a2ccd2340ae
_Avvake_ <denisergo18@gmail.com>
2025-02-17 22:39:28 +0200

    visual update

--- src/api/users/decorators.py
+++ src/api/users/decorators.py
@@ -1,6 +1,11 @@
-from functools import wraps
-from src.exceptions import UserAlreadyExistsError, TokenIsNotValidError, ServiceError
 from fastapi import HTTPException, status
+from functools import wraps
+from src.exceptions import (
+    UserAlreadyExistsError,
+    TokenIsNotValidError,
+    ServiceError,
+    NotFoundError,
+)
 
 
 def handle_users_error_decorator(func):
@@ -8,7 +13,12 @@ def handle_users_error_decorator(func):
     async def wrapper(*args, **kwargs):
         try:
             return await func(*args, **kwargs)
-        except (TokenIsNotValidError, UserAlreadyExistsError, ServiceError) as error:
+        except (
+            TokenIsNotValidError,
+            UserAlreadyExistsError,
+            ServiceError,
+            NotFoundError,
+        ) as error:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail=str(error)
             )
