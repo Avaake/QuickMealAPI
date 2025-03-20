dbbe44c549d45727dd0bf55a5cabde756b5aa6d8
_Avvake_ <denisergo18@gmail.com>
2025-03-20 23:23:06 +0200

    updated cart service

--- src/services/cart_service.py
+++ src/services/cart_service.py
@@ -1,36 +1,38 @@
-from src.schemas.cart_schemas import BaseCartSchema, UpdateCartSchema
+from src.schemas.cart_schemas import (
+    BaseCartSchema,
+    ReadCartDishSchema,
+)
 from src.repositories.cart_repository import CartRepository
 from src.services.base_service import AbstractService
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import Sequence
 from src.core import db_helper, CartItem
 from typing import Annotated
 from fastapi import Depends
 
 
-class DishService(AbstractService):
+class CartService(AbstractService):
     def __init__(
         self, session: Annotated[AsyncSession, Depends(db_helper.get_async_session)]
     ):
         self._cart_repository = CartRepository(session)
 
-    async def add(self, cart_item_data: BaseCartSchema):
+    async def add(self, user_id: int, cart_data: BaseCartSchema):
         return await self._cart_repository.add_item(
-            cart_item_data.user_id, cart_item_data.dish_id, cart_item_data.quantity
+            user_id, cart_data.dish_id, cart_data.quantity
         )
 
-    async def update(self, cart_item_data: UpdateCartSchema):
-        return await self._cart_repository.delete(
-            user_id=cart_item_data.user_id, dish_id=cart_item_data.dish_id
-        )
+    async def update(self, user_id: int, dish_id: int):
+        return await self._cart_repository.delete(user_id=user_id, dish_id=dish_id)
 
     async def delete(self, user_id: int) -> None:
         return await self._cart_repository.delete(user_id=user_id)
 
-    async def get(self, user_id: int) -> Sequence[CartItem]:
-        return await self._cart_repository.find_cart(user_id=user_id)
+    async def get(self, user_id: int) -> list[ReadCartDishSchema]:
+        cart = await self._cart_repository.find_cart(user_id=user_id)
+        return [
+            ReadCartDishSchema(dish_id=i.dish_id, quantity=i.quantity, name=i.dish.name)
+            for i in cart
+        ]
 
     async def calculate_total(self, user_id: int):
-        # cart_items = await self.get(user_id=user_id)
-        # return sum(item.quantity * item.dish.price for item in cart_items)
         return await self._cart_repository.calculate_total(user_id=user_id)
