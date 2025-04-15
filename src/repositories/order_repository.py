7c9640f789abe75fe491d1221417e698cfc9091b
_Avvake_ <denisergo18@gmail.com>
2025-04-15 22:45:24 +0300

    created order repository

---
+++ src/repositories/order_repository.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from sqlalchemy import select
+
+from src.repositories.sqlalchemy_repository import SQLAlchemyRepository, ModelType
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.core import Order, OrderItem
+from sqlalchemy.orm import selectinload
+
+
+class OrderRepository(SQLAlchemyRepository[Order]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(Order, session)
+
+    @staticmethod
+    def create_order_items_instance(
+        dish_id: int, quantity: int, price: int
+    ) -> OrderItem:
+        return OrderItem(
+            dish_id=dish_id,
+            quantity=quantity,
+            price=price,
+        )
+
+    async def find_single(self, **filters) -> Union[ModelType, None]:
+        res = await self._session.execute(
+            select(self.model)
+            .options(selectinload(self.model.items))
+            .filter_by(**filters)
+        )
+        return res.scalar_one_or_none()
