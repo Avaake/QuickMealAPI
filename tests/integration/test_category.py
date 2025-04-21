9a651ad9d7a23ddfd5dced2b3765f5794460dbe2
_Avvake_ <denisergo18@gmail.com>
2025-04-21 23:28:18 +0300

    update test_category

--- tests/integration/test_category.py
+++ tests/integration/test_category.py
@@ -4,193 +4,157 @@ from src.core import Category
 
 
 @pytest.mark.asyncio
-async def test_create_new_category(async_client, admin_user_token, db_session):
-    category_data = {
-        "name": "Test Category",
-        "description": "Test description",
-    }
+class TestCategory:
 
-    response = await async_client.post(
-        "/categories",
-        json=category_data,
-        headers={
-            "Authorization": f"Bearer {admin_user_token['access_token']}",
-        },
-    )
+    async def test_create_new_category(
+        self, async_client, authorization_admin, db_session, category_data
+    ):
+        response = await async_client.post(
+            "/categories",
+            json=category_data,
+            headers=authorization_admin,
+        )
 
-    assert response.status_code == 201
-    data = response.json()
+        assert response.status_code == 201
+        data = response.json()
 
-    assert "id" in data
-    assert data["name"] == "Test Category"
-    assert data["description"] == "Test description"
+        assert "id" in data
+        assert data["name"] == "Test Category"
+        assert data["description"] == "Test description"
 
-    stmt = select(Category).where(Category.id == data["id"])
-    result = await db_session.execute(stmt)
-    category = result.scalars().first()
+        stmt = select(Category).where(Category.id == data["id"])
+        result = await db_session.execute(stmt)
+        category = result.scalars().first()
 
-    assert category is not None
+        assert category is not None
 
+    async def test_create_category_forbidden_not_enough_rights(
+        self, async_client, authorization_homer, category_data
+    ):
+        response = await async_client.post(
+            "/categories",
+            json=category_data,
+            headers=authorization_homer,
+        )
 
-@pytest.mark.asyncio
-async def test_create_category_forbidden_not_enough_rights(
-    async_client, homer_user_token
-):
-    category_data = {
-        "name": "Test Category",
-        "description": "Test description",
-    }
+        assert response.status_code == 403
+        data = response.json()
+        assert data["detail"] == "Not enough rights"
 
-    response = await async_client.post(
-        "/categories",
-        json=category_data,
-        headers={
-            "Authorization": f"Bearer {homer_user_token['access_token']}",
-        },
-    )
+    async def test_create_category_validate_error(
+        self, async_client, authorization_admin
+    ):
 
-    assert response.status_code == 403
-    data = response.json()
-    assert data["detail"] == "Not enough rights"
+        response = await async_client.post(
+            "/categories",
+            json={"description": "Test description"},
+            headers=authorization_admin,
+        )
 
+        assert response.status_code == 422
 
-@pytest.mark.asyncio
-async def test_create_category_validate_error(async_client, admin_user_token):
-    category_data = {
-        "description": "Test description",
-    }
+    async def test_get_all_categories(self, async_client):
+        response = await async_client.get("/categories")
 
-    response = await async_client.post(
-        "/categories",
-        json=category_data,
-        headers={
-            "Authorization": f"Bearer {admin_user_token['access_token']}",
-        },
-    )
+        assert response.status_code == 200
 
-    assert response.status_code == 422
+        data = response.json()
 
+        assert isinstance(data, list)
+        assert len(data) > 1
 
-@pytest.mark.asyncio
-async def test_get_all_categories(async_client):
-    response = await async_client.get("/categories")
+    async def test_get_category(self, async_client):
+        response = await async_client.get("/categories/3")
 
-    assert response.status_code == 200
+        assert response.status_code == 200
 
-    data = response.json()
+        data = response.json()
 
-    assert isinstance(data, list)
-    assert len(data) > 1
+        assert data["id"] == 3
+        assert data["name"] == "Test Category"
+        assert data["description"] == "Test description"
 
+    async def test_update_category(
+        self, async_client, authorization_admin, db_session, new_category_data
+    ):
+        response = await async_client.patch(
+            "/categories/3",
+            json=new_category_data,
+            headers=authorization_admin,
+        )
 
-@pytest.mark.asyncio
-async def test_get_category(async_client):
-    response = await async_client.get("/categories/3")
+        assert response.status_code == 200
+        data = response.json()
 
-    assert response.status_code == 200
+        assert data["id"] == 3
+        assert data["name"] == "New Test Category"
 
-    data = response.json()
+        stmt = select(Category).where(Category.id == data["id"])
+        result = await db_session.execute(stmt)
+        category = result.scalars().first()
 
-    assert data["id"] == 3
-    assert data["name"] == "Test Category"
-    assert data["description"] == "Test description"
+        assert category.id == data["id"]
+        assert category.name == "New Test Category"
 
+    async def test_update_category_forbidden_not_enough_rights(
+        self, async_client, authorization_homer, new_category_data
+    ):
+        response = await async_client.patch(
+            "/categories/3",
+            json=new_category_data,
+            headers=authorization_homer,
+        )
 
-@pytest.mark.asyncio
-async def test_update_category(async_client, admin_user_token, db_session):
-    category_data = {
-        "name": "New Test Category",
-    }
-    response = await async_client.patch(
-        "/categories/3",
-        json=category_data,
-        headers={"Authorization": f"Bearer {admin_user_token['access_token']}"},
-    )
+        assert response.status_code == 403
+        data = response.json()
+        assert data["detail"] == "Not enough rights"
 
-    assert response.status_code == 200
-    data = response.json()
+    async def test_update_category_not_found_error(
+        self, async_client, authorization_admin, new_category_data
+    ):
+        response = await async_client.patch(
+            "/categories/22",
+            json=new_category_data,
+            headers=authorization_admin,
+        )
 
-    assert data["id"] == 3
-    assert data["name"] == "New Test Category"
+        assert response.status_code == 400
+        data = response.json()
+        assert data["detail"] == "Category not found"
 
-    stmt = select(Category).where(Category.id == data["id"])
-    result = await db_session.execute(stmt)
-    category = result.scalars().first()
+    async def test_delete_category(self, async_client, authorization_admin, db_session):
+        response = await async_client.delete(
+            "/categories/3",
+            headers=authorization_admin,
+        )
 
-    assert category.id == data["id"]
-    assert category.name == "New Test Category"
+        assert response.status_code == 204
 
+        stmt = select(Category).where(Category.id == 3)
+        result = await db_session.execute(stmt)
+        category = result.scalars().first()
 
-@pytest.mark.asyncio
-async def test_update_category_forbidden_not_enough_rights(
-    async_client, homer_user_token
-):
-    category_data = {
-        "name": "New Test Category",
-    }
-    response = await async_client.patch(
-        "/categories/3",
-        json=category_data,
-        headers={"Authorization": f"Bearer {homer_user_token['access_token']}"},
-    )
+        assert category is None
 
-    assert response.status_code == 403
-    data = response.json()
-    assert data["detail"] == "Not enough rights"
+    async def test_delete_category_forbidden_not_enough_rights(
+        self, async_client, authorization_homer
+    ):
+        response = await async_client.delete(
+            "/categories/3",
+            headers=authorization_homer,
+        )
+        assert response.status_code == 403
+        data = response.json()
+        assert data["detail"] == "Not enough rights"
 
+    async def test_delete_category_not_found_error(
+        self, async_client, authorization_admin
+    ):
+        response = await async_client.delete(
+            "/categories/22",
+            headers=authorization_admin,
+        )
 
-@pytest.mark.asyncio
-async def test_update_category_not_found_error(async_client, admin_user_token):
-    category_data = {
-        "name": "New Test Category",
-    }
-    response = await async_client.patch(
-        "/categories/22",
-        json=category_data,
-        headers={"Authorization": f"Bearer {admin_user_token['access_token']}"},
-    )
-
-    assert response.status_code == 400
-    data = response.json()
-    assert data["detail"] == "Category not found"
-
-
-@pytest.mark.asyncio
-async def test_delete_category(async_client, admin_user_token, db_session):
-    response = await async_client.delete(
-        "/categories/3",
-        headers={"Authorization": f"Bearer {admin_user_token['access_token']}"},
-    )
-
-    assert response.status_code == 204
-
-    stmt = select(Category).where(Category.id == 2)
-    result = await db_session.execute(stmt)
-    category = result.scalars().first()
-
-    assert category is None
-
-
-@pytest.mark.asyncio
-async def test_delete_category_forbidden_not_enough_rights(
-    async_client, homer_user_token
-):
-    response = await async_client.delete(
-        "/categories/3",
-        headers={"Authorization": f"Bearer {homer_user_token['access_token']}"},
-    )
-    assert response.status_code == 403
-    data = response.json()
-    assert data["detail"] == "Not enough rights"
-
-
-@pytest.mark.asyncio
-async def test_delete_category_not_found_error(async_client, admin_user_token):
-    response = await async_client.delete(
-        "/categories/22",
-        headers={"Authorization": f"Bearer {admin_user_token['access_token']}"},
-    )
-
-    assert response.status_code == 400
-    data = response.json()
-    assert data["detail"] == "Category not found"
+        assert response.status_code == 400
+        data = response.json()
+        assert data["detail"] == "Category not found"
